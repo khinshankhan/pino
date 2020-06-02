b1c7fff416690a936d522d463f15208f32936b18
woojiahao <woojiahao1234@gmail.com>
2020-06-02 10:54:57 +0800

    Add .env generation script
    Run the script with `bash scripts/create-env.sh`

---
+++ scripts/create-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read -p 'MonogDB username >>> ' username
+read -p 'MongoDB password >>> ' password
+
+{
+  echo "MONGO_INITDB_ROOT_USERNAME=$username"
+  echo "MONGO_INITDB_ROOT_PASSWORD=$password"
+  echo "MONGO_INITDB_DATABASE=pino"           # Might want to customise this?
+  echo "ME_CONFIG_MONGODB_ADMINUSERNAME=$username"
+  echo "ME_CONFIG_MONGODB_ADMINPASSWORD=$password"
+} >> ./.env
