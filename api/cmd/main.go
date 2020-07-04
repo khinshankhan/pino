879b78665126d59af1e259b473c5c4d0565d62ce
woojiahao <woojiahao1234@gmail.com>
2020-07-04 11:30:47 +0800

    Add logging for API start

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"log"
 	"net/http"
 )
 
+const (
+	port = 8080
+)
+
 func main() {
+	log.Print("Starting server")
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte("welcome"))
 	})
-	_ = http.ListenAndServe(":3000", r)
+	log.Printf("Connected to port %d. Access at http://localhost:%d", port, port)
+	_ = http.ListenAndServe(fmt.Sprintf(":%d", port), r)
 }
